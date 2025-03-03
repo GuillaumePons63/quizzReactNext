67c5e6705ba3354139e1c81c8338b802b4d0444c
GuillaumePons63 <guillaume@web-63.fr>
2025-03-03 22:44:56 +0100

    design button nav

--- quizz/src/app/quizz/component/numberQuestion.js
+++ quizz/src/app/quizz/component/numberQuestion.js
@@ -8,8 +8,9 @@ export default function NumberQuestion({ setNumber, numberTotalQuestions,current
         setNumber(prev => prev - 1);
     }
    
-    return <div>        
-        { currentNumber > 0 && <button onClick={removeNumberQuestions}>Revenir à la question précédente </button>}
-        { currentNumber < numberTotalQuestions && <button onClick={addNumberQuestions}>Passer à la prochaine question </button>}
+    return <div>
+         { currentNumber < numberTotalQuestions && <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded m-3 " onClick={addNumberQuestions}> &lt;&lt; </button>}        
+        { currentNumber > 0 && <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded m-3" onClick={removeNumberQuestions}>  &gt;&gt; </button>}
+       
     </div>
 }
